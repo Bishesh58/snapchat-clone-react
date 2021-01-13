7722b766ec516b2fd63755fe496c4b27fe782ef9
Bishesh <bishesh.sunam@gmail.com>
2021-01-13 16:17:43 +1300

    styled preview screen

--- src/Components/Preview.js
+++ src/Components/Preview.js
@@ -3,7 +3,15 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { resetCameraImage, selectCameraImage } from '../features/cameraSlice';
 import './Preview.css';
-import CloseIcon from '@material-ui/icons/Close';
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
+import TextFieldsIcon from "@material-ui/icons/TextFields";
+import CreateIcon from "@material-ui/icons/Create";
+import NoteIcon from "@material-ui/icons/Note";
+import MusicNoteIcon from "@material-ui/icons/MusicNote";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
+import CropIcon from "@material-ui/icons/Crop";
+import TimerIcon from "@material-ui/icons/Timer";
+import SendIcon from "@material-ui/icons/Send";
 
 
 function Preview() {
@@ -25,8 +33,21 @@ function Preview() {
     }
     return (
         <div className='preview'>
-            <CloseIcon onClick={closePreview} className='preview__close' />
+            <HighlightOffIcon onClick={closePreview} className='preview__close' />
+            <div className="preview__toolbarRight">
+                <TextFieldsIcon />
+                <CreateIcon />
+                <NoteIcon />
+                <MusicNoteIcon />
+                <AttachFileIcon />
+                <CropIcon />
+                <TimerIcon />
+            </div>
             <img src={cameraImage} alt=""/>
+            <div className="preview__footer">
+                <h2>Send Now</h2>
+                <SendIcon fontSize='small' className='preview__sendIcon' />
+            </div>
         </div>
     )
 }
